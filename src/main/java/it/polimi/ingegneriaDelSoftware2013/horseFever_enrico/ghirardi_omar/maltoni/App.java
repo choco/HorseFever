215c886901d6c298ef0b75ec7da110552fcccd4c
enrico.ghirardi <enrico.ghirardi@76c776c7-529a-4c13-b958-51bb11ea1662>
2013-04-18 19:37:54 +0000

    Primo commit

---
+++ src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_enrico/ghirardi_omar/maltoni/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingegneriaDelSoftware2013.horseFever_enrico.ghirardi_omar.maltoni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
