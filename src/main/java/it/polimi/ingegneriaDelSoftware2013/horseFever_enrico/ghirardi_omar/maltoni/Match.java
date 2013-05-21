8a965e596a4ff916bafe4f44dcb98c1d1293e079
enrico.ghirardi <enrico.ghirardi@76c776c7-529a-4c13-b958-51bb11ea1662>
2013-05-21 13:17:37 +0000

    Initial implementation of the Match class, including some functions for the game flow

--- src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_enrico/ghirardi_omar/maltoni/Match.java
+++ src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_enrico/ghirardi_omar/maltoni/Match.java
@@ -1,7 +1,7 @@
 package it.polimi.ingegneriaDelSoftware2013.horseFever_enrico.ghirardi_omar.maltoni;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,7 +19,9 @@ public abstract class Match {
     private Deck movementCardDeck;
     private Deck actionCardDeck;
 
-    /* mazzi board game
+    private BetManager betManager;
+
+    // mazzi board game
 
     private Deck characterCardDeck;
     private Deck stableCardDeck;
@@ -27,29 +29,165 @@ public abstract class Match {
     private Deck employerCardDeck; //mazzo carte aiutanti
     private Deck goalCardDeck;
     //come implementare le carte debito?
-
-    */
     //...
 
-    public Match (){
+    public Match() {
         // riceve dati da interfaccia o lancia lui l'interfaccia?  inizializza il gioco o è l'istanza dei turni? ... lo lancia HorseFever
         //inizializza le variabili dei mazzi con la chiamata del costruttore
+
+        //inizializzi i mazzi
+        // inizializzi la blackboard
+        //inizializzi bet manager
+        betManager = new BetManager();
+        //inizializzi lane
+        //inizializzi race manager;
+
     }
 
-   /* private int getNumberOfPlayers (){ //lancia interfaccia grafica e compila la lista giocatori.. eccezione da gestire se vengono aggiunti più di 6 e meno di 2 giocatori
+    public void startMatch() {
+        giveCharacterCards();
+        setFirstPlayer();
+        /* se è family game */
+        giveActionCards(2);
 
-    } */
+        betPhase(true);
+        rigPhase();
+        betPhase(false);
 
-    private int getNumberOfTurns (int numberOfPlayers){
-          if (numberOfPlayers==2) return 6;
-          if (numberOfPlayers==3) return 6;
-          if (numberOfPlayers==4) return 4;
-          if (numberOfPlayers==5) return 5;
-          if (numberOfPlayers==6) return 6;
-          return -1;
-          }
+        racePhase();
+
+    }
+
+    private void racePhase() {
+
+    }
+
+    private void rigPhase() {
+        //sempre questione del giro orario che parte dal first player da risolvere
+        while (someoneStillHasActionCards()) {
+            for (Player player : players) {
+                if (!player.isActionCardPileEmpty()) {
+                    //chiamiamo interfaccia per determinare carta e lane su cui giocare la carta stessa
+                    player.playActionCard(card, lane);
+                }
+            }
+        }
+    }
+
+    private boolean someoneStillHasActionCards() {
+        for (Player player : players) {
+            if (!player.isActionCardPileEmpty())
+                return true;
+        }
+        return false;
+    }
+
+    private boolean someoneStillHasBetsToMake() {
+        //da implementare
+        return false;
+    }
+
+    private void betPhase(boolean mandatory) {
+        /*per family game e quasi anche per il boardgame */
+
+        //prima fase
+
+        // bisogna implementare il problema di compiere questo for in senso antiorario o orario in base alla variabile mandatory (true = orario, false = antiorario)
+        for (Player player : players) {   //senso orario
+            if (!mandatory) {
+                // possibilita di uscire dalla funzione e non farla
+                /*
+                if(false) { //se ha scelto di non farla
+                    continue;
+                }                  */
+            }
+            //chiama interfaccia e chiede al giocatore i valori per la makeBet
 
 
+            boolean betCorrectlyMade = false;
+            while (!betCorrectlyMade) {
+                Bet playerBet;
 
+                try {
+                    playerBet = player.makeBet(amount, type, lane);
+                } catch () {
+
+                }
+
+                if (betManager.insertBet(playerBet)) {
+                    betCorrectlyMade = true;
+                }
+
+            }
+        }
+
+
+    }
+
+    private void giveActionCards(int numCards) {
+        /* assegnamento temporaneo in attesa di struttura migliore per gestire i players */
+        for (Player player : players) {
+            for (int i = 0; i < numCards; i++)
+                player.addActionCard((ActionCard) actionCardDeck.draw());
+        }
+    }
+
+    private void setFirstPlayer() {
+        Random generator = new Random();
+        players.get(generator.nextInt(players.size() - 1)).setFirstPlayer(true);
+    }
+
+    private void giveCharacterCards() {
+        for (Player player : players) {
+            player.setCharCard((CharacterCard) characterCardDeck.draw());
+        }
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    private int getNumberOfPlayers() { //lancia interfaccia grafica e compila la lista giocatori.. eccezione da gestire se vengono aggiunti più di 6 e meno di 2 giocatori
+        return players.size();
+    }
+
+    private int getNumberOfTurns() {
+        switch (players.size()) {
+            case 2:
+            case 3:
+            case 6:
+                return 6;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            default:
+                return -1;
+        }
+
+    }
+
+    private int numberOfMarksPerColor() {
+        switch (players.size()) {
+            case 2:
+                return 1;
+            case 3:
+                return 2;
+            case 4:
+                return 3;
+            case 5:
+            case 6:
+                return 4;
+            default:
+                return -1;
+        }
+
+    }
+
+    static void main() {
+        Match partita = new Match();
+        partita.addPlayer(player);
+        partita.startMatch();
+    }
 }
 
